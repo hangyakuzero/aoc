597153fc3dce2b382a4e380891d02135618d1c0d
Hangyaku <hangyaku@Hangyakus-MacBook-Pro.local>
2024-12-03 00:21:01 +0530

    yeno ondu

---
+++ 2.py
@@ -0,0 +1,24 @@
+l = []
+k = []
+flag = 0
+flag1 = 0
+with open("day2.txt", "r") as f1:
+    lines = f1.read().splitlines()
+
+    print(lines)
+    for i in lines:
+        k = i.split()
+        for j in range(len(k)):
+            k[j] = int(k[j])
+
+        if sorted(k) == k or sorted(k, reverse=True) == k:
+            # print(k)
+            for n in range(len(k) - 1):
+                diff = abs(k[n] - k[n + 1])
+                if diff != 0 and diff <= 3:
+                    flag += 1
+
+            if flag == len(k) - 1:
+                flag1 += 1
+            flag = 0
+print(flag1)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # aoc
+## this is a repo I created to upload all my solutions for aoc 2024
+---
+coding in python is actually super fun, maybe I'll use some exotic or esoteric language in the future lol, but for now, python it is.
+
+*Visit again, ly*
