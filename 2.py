a23f22bdb3a57f918dacdc4ad1475d15f6f4566c
Hangyaku <hangyakuzero@gmail.com>
2024-12-27 15:14:22 +0530

    adding aoc stuff

--- 2.py
+++ 2.py
@@ -2,7 +2,7 @@ l = []
 k = []
 flag = 0
 flag1 = 0
-with open("day2.txt", "r") as f1:
+with open("samp.txt", "r") as f1:
     lines = f1.read().splitlines()
 
     print(lines)
@@ -21,4 +21,20 @@ with open("day2.txt", "r") as f1:
             if flag == len(k) - 1:
                 flag1 += 1
             flag = 0
+
+        else:
+            for i in range(len(k)):
+                k.pop(i)
+                if sorted(k) == k or sorted(k, reverse=True) == k:
+                    # print(k)
+                    for n in range(len(k) - 1):
+                        diff = abs(k[n] - k[n + 1])
+                        if diff != 0 and diff <= 3:
+                            flag += 1
+
+                if flag == len(k) - 1:
+                    flag1 += 1
+            flag = 0
+
+
 print(flag1)
